859791102e8d44d6c0b4045ff667c8e010aa8454
Henry Adam <iihenymii@gmail.com>
2024-11-27 11:13:31 -0300

    erro overflow resolvido

--- src/Components/Mario.jsx
+++ src/Components/Mario.jsx
@@ -3,10 +3,68 @@ import "../css/Mario.css"
 
 function Mario() {
 
-    return(
+    return (
         <div className="slide-mario">
-            <h1>Super Mario Odyssey</h1>
-            <p>Teste navegação slides</p>
+            <div className="banner-mario">
+                {/* <!-- Banner de conteúdo adicional --> */}
+                <div className="left">
+                    {/* <!-- Seção à esquerda do banner --> */}
+                    <div className="features">
+                        {/* <!-- Div para exibir as características do jogo --> */}
+                        <div className="rating">
+                            {/* <!-- Exibe a nota de avaliação do jogo --> */}
+                            <span className="score">97</span>
+                            <div className="content">
+                                <span className="subtitle">M E T A S C O R E</span>
+                                <h4>Aprovação Universal</h4>
+                                <span>Explore lugares incríveis longe do Reino Cogumelo com o Mario e o novo aliado Cappy em uma imensa aventura 3D ao redor do mundo. Use novas habilidades incríveis para coletar Power Moons, ligar a aeronave Odyssey e salvar a princesa Peach de se casar com o Bowser!</span>
+                            </div>
+                        </div>
+                        <div className="pricing">
+                            {/* <!-- Seção para exibir informações de compra --> */}
+                            <a href=""></a>
+                            {/* <!-- Link vazio para a compra do jogo --> */}
+                            <div className="content">
+                                {/* <!-- Conteúdo relacionado ao preço --> */}
+                                <span className="subtitle">Super Mario Odyssey</span>
+                                <h4>COMPRE AGORA</h4>
+                                <span>Plataforma, Ação</span>
+                            </div>
+                        </div>
+                    </div>
+                    <h1>Super Mario Odyssey</h1>
+                </div>
+
+                {/* <img src="images/mario.png" alt="Mario" /> */}
+
+                    {/* <!-- Imagem do personagem Mario --> */}
+                    <div className="right">
+                        {/* <!-- Seção à direita do banner --> */}
+                        <ul className="description">
+                            {/* <!-- Lista de descrição do jogo --> */}
+                            <li>
+                                Data de lançamento
+                                <p>27 de Outubro 2017</p>
+                                {/* <!-- Data de lançamento do jogo --> */}
+                            </li>
+                            <li>
+                                MODOS DE JOGO
+                                <p>Modo TV, Modo Semiportátil, Modo Portátil</p>
+                                {/* <!-- Modos de jogo disponíveis --> */}
+                            </li>
+                        </ul>
+
+                        <div className="trailer">
+                            {/* <!-- Seção para exibir o trailer --> */}
+                            <h2>Assista ao Trailer</h2>
+                            {/* <!-- Título "Assista ao Trailer" --> */}
+                            <iframe width="1280" height="720" src="https://www.youtube.com/embed/5kcdRBHM7kM"
+                                title="Super Mario Odyssey Trailer" allowFullScreen></iframe>
+                            {/* <!-- Frame do YouTube com o trailer do Super Mario Odyssey --> */}
+                        </div>
+
+                    </div>
+            </div>
         </div>
     )
 }
--- src/Components/Slider.jsx
+++ src/Components/Slider.jsx
@@ -1,19 +1,31 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import Mario from "./Mario";
 import Pokemon from "./Pokemon";
 import Zelda from "./Zelda";
-import "../css/Slider.css"   
+
+import bgMario from "../assets/bg-mario.jpeg"
+import bgPokemon from "../assets/bg-pokemon.jpeg"
+import bgZelda from "../assets/bg-zelda.jpeg"
+import "../css/Slider.css"
 
 function Slider() {
 
     const [currentIndex, setCurrentIndex] = useState(0)
 
-    // Dados dos slides: cada objeto contém o componente do conteúdo e a imagem
+    // Dados dos slides
     const slides = [
-        <Mario />,
-        <Pokemon />,
-        <Zelda />,
+        { component: <Mario />, background: `url(${bgMario})` },
+        { component: <Pokemon />, background: `url(${bgPokemon})` },
+        { component: <Zelda />, background: `url(${bgZelda})` },
     ];
+
+    // Atualiza a imagem de fundo do body quando o slide muda
+    useEffect(() => {
+        document.body.style.backgroundImage = slides[currentIndex].background;
+        document.body.style.backgroundSize = 'cover';  // Garante que a imagem cubra toda a tela
+        document.body.style.backgroundPosition = 'center';  // Centraliza a imagem
+        document.body.style.backgroundAttachment = 'fixed';  // A imagem ficará fixa ao rolar
+    }, [currentIndex]); // Efeito é acionado sempre que currentIndex mudar
 
     // Função para ir para o slide anterior
     const prevSlide = () => {
@@ -35,7 +47,7 @@ function Slider() {
                 &lt;
             </button>
 
-            <div className="slider">{slides[currentIndex]}
+            <div className="slider">{slides[currentIndex].component}
             </div>
 
             <button className="next" onClick={nextSlide}>
